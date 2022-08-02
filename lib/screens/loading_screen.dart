efc18320a1db1f6e14e31e53069e8cffa701c2e1
Tejas Shelke <tejasshelke040402@gmail.com>
2022-08-02 23:40:22 +0530

    Project Commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:clima/services/weather.dart';
+
+
+
+const apiKey = 'e044b4010a00b3c5e25ac54a91118658';
+
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  double longitude;
+  double latitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+
+  void getLocationData() async {
+
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = await weatherModel.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(locationWeather: weatherData,);
+    }));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitWave(
+          color: Colors.white,
+          size: 100.0,
+        )
+      ),
+    );
+  }
+}
